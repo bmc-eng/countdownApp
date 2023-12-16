4db6e2c3f24ba48e3c39ffad08480a4edac2129d
bmc-eng <bclarkeuk@googlemail.com>
2023-12-16 23:05:44 +0000

    startscreen initial

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Created pages & Components
+import StartScreen from './pages/startscreen';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <StartScreen />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#3B87C4",
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ pages/startscreen.js
@@ -0,0 +1,16 @@
+import { StyleSheet, View } from 'react-native'
+
+
+function StartScreen() {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default StartScreen
