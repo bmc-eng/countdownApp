2ae57d6e0331ab3c04b884de6b1861010165c569
bmc-eng <bclarkeuk@googlemail.com>
2023-12-17 22:11:45 +0000

    added the letter box into project

---
+++ components/letterbox.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { TextInput, View, StyleSheet } from "react-native";
+
+function LetterBox({ onInput }) {
+
+    const [enteredLetter, setEnteredLetter] = useState('')
+
+    function onChangeText(enteredLetter) {
+        setEnteredLetter(enteredLetter)
+        console.log(enteredLetter)
+    }
+
+    return (
+        <View style={styles.outerContainer}>
+            <View style={styles.innerContainer}>
+            <TextInput style={styles.textBox}
+                maxLength={1}
+                onChangeText={onChangeText}
+                value={enteredLetter} 
+            />
+            </View>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    outerContainer: {
+        backgroundColor: '#095b9e',
+        width: 100,
+        height: 100,
+        padding: 5
+    },
+    innerContainer: {
+        flex:1,
+        backgroundColor: '#044479'
+    },
+    textBox: {
+        flex: 1,
+        fontSize: 60,
+        padding: 15,
+        textAlign: 'center',
+        color: 'white',
+        fontWeight: 'bold',
+        fontFamily: 'Futura-Bold'
+    }
+
+})
+
+export default LetterBox
--- pages/startscreen.js
+++ pages/startscreen.js
@@ -1,16 +1,17 @@
 import { StyleSheet, View } from 'react-native'
+import LetterBox from '../components/letterbox'
 
 
 function StartScreen() {
     return (
         <View>
-
+            <LetterBox />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    
+
 })
 
 export default StartScreen
