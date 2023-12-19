01fc997d6751c5502a1ce21f9e8b8b4ac28ddce2
bmc-eng <bclarkeuk@googlemail.com>
2023-12-19 21:57:47 +0000

    added image to start screen

--- pages/startscreen.js
+++ pages/startscreen.js
@@ -1,18 +1,35 @@
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Image, SafeAreaView } from 'react-native'
 import LetterBox from '../components/letterbox'
 import LetterHolder from '../components/letterholder'
 
 
 function StartScreen() {
     return (
-        <View>
+        <SafeAreaView>
+        <View style={styles.imageContainer}>
+            <Image style={styles.image} source={require('../assets/images/countdownTitle.png')}/>
+        </View>
+        <View style={styles.mainContainer}>
             <LetterHolder />
         </View>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    
+    imageContainer: {
+        flex:1
+    },
+    image: {
+        width: 290,
+        height: 190,
+        margin: 20,
+        marginHorizontal:80,
+        justifyContent: 'center'
+    },
+    mainContainer: {
+        flex: 7
+    }
 })
 
 export default StartScreen
